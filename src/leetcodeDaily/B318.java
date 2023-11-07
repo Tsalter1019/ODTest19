10f39dae6b4e35b7e8c3b3094beb8f6ae3d4001d
Tsalter1019 <zhikang1019@163.com>
2023-11-07 08:51:33 +0800

    daily practise 20231106 and 代码块的执行顺序题

--- src/leetcodeDaily/B318.java
+++ src/leetcodeDaily/B318.java
@@ -1,8 +1,49 @@
 package leetcodeDaily;
 
-
+//给你一个字符串数组 words ，找出并返回 length(words[i]) * length(words[j]) 的最大值，并且这两个单词不含有公共字母
+//。如果不存在这样的两个单词，返回 0 。
+// 示例 1：
+//输入：words = ["abcw","baz","foo","bar","xtfn","abcdef"]
+//输出：16
+//解释：这两个单词为 "abcw", "xtfn"。
+// 示例 2：
+//输入：words = ["a","ab","abc","d","cd","bcd","abcd"]
+//输出：4
+//解释：这两个单词为 "ab", "cd"。
+// 示例 3：
+//输入：words = ["a","aa","aaa","aaaa"]
+//输出：0
+//解释：不存在这样的两个单词。
 public class B318 {
+    public static void main(String[] args) {
+        String[] str = {"abcw","baz","foo","bar","xtfn","abcdef"};
+        System.out.println(new B318().maxProduct(str));
+    }
     public int maxProduct(String[] words) {
-        return 0;
+        int max=0;
+        for(int i=0;i<words.length;i++){
+            for(int j=i+1;j<words.length;j++){
+                if(!isContainCommonLetter(words[i],words[j])){
+                    int mul = words[i].length()*words[j].length();
+                    if(max<mul){
+                        max = mul;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+    public static boolean isContainCommonLetter(String str1,String str2){
+        int count=0;
+        for(int i=0;i<str2.length();i++){
+            if(str1.contains(str2.charAt(i)+"")){
+                count++;
+            }
+        }
+        if(count>0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
---
+++ src/test/java/Person.java
@@ -0,0 +1,25 @@
+package test.java;
+
+//  代码块的执行顺序题
+public class Person {
+    private static int count;
+    private String name;
+    public Person(){
+        count++;//创建对象p1-------2--c     //创建对象p2-------3--c
+        System.out.println("Constructor executed.");
+    }
+    {
+        name = "John";//创建对象p1-------2--b    //创建对象p2-------3--b
+        System.out.println("Non-static code block executed.");
+    }
+    static {//首先执行------1
+        count = 1;
+        System.out.println("Static code block executed.");
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person();//创建对象p1-------2--a
+        Person p2 = new Person();//创建对象p2-------3--a
+        System.out.println("Total count: " + count);//最后输出
+    }
+}
