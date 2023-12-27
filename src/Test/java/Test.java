bd1993ec607a8ff593443da80b363a55f8324386
Tsalter1019 <zhikang1019@163.com>
2023-12-27 16:51:54 +0800

    daily practise

--- src/Test/java/Test.java
+++ src/Test/java/Test.java
@@ -1,28 +1,82 @@
 package Test.java;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
-    public int unequalTriplets(int[] nums) {
-        int count=0;
-        int l = nums.length;
-        for(int i=0;i<l;i++){
-            for(int j=i+1;j<l;j++){
-                for(int k=j+1;k<l;k++){
-                    if(nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]){
-                        count++;
-                    }
-                }
+    public static void main(String[] args) {
+        //System.out.println(isStrEquOrNot("高层住宅,多层住宅,装饰","多层住宅,高层住宅,装饰"));
+        solution();
+    }
+    //        Object[] obj = {"高层住宅,多层住宅","装饰","2"};
+    //        Object[] obj1 = {"多层住宅","主体","1"};
+    //        Object[] obj2 = {"多层住宅,高层住宅","主体","1"};
+    public static void solution(){
+        Object[] obj = {"高层住宅,多层住宅","装饰",1};
+        Object[] obj1 = {"多层住宅","主体",1};
+        Object[] obj2 = {"多层住宅,高层住宅","主体",1};
+        Object[] obj3 = {"多层住宅,高层住宅","装饰",1};
+
+        ArrayList<Object []> list = new ArrayList<>();
+        list.add(obj);
+        list.add(obj1);
+        list.add(obj2);
+        list.add(obj3);
+
+        String[] str1 = new String[list.size()];
+        String[] str2 = new String[list.size()];
+        String[] str3 = new String[list.size()];
+
+        int[] int3 = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            str1[i] = list.get(i)[0].toString();
+            str2[i] = list.get(i)[1].toString();
+            int3[i] = (int) list.get(i)[2];
+        }
+        Map<String,Integer> map = new HashMap<>();
+        Map<String,Integer> map1 = new HashMap<>();
+
+        for(int i=0;i<list.size();i++){
+            str3[i] = str1[i]+","+str2[i];
+            map.put(str3[i],int3[i]);
+        }
+
+        for(String s:map.keySet()){
+            if(!map1.containsKey(s) && isMapContainsKey(s,map1)){
+                map1.put(s,map.get(s));
             }
         }
-        return count;
+
+
+
+        System.out.println(11);
+
     }
 
-    public static void main(String[] args) {
-        int[] arr1 = {4,4,2,4,3};
-        String str = "中天aabbcccc";
-        char[] ch = str.toCharArray();
-        System.out.println(ch);
+    private static boolean isMapContainsKey(String str,Map<String,Integer> map){
+        int count=0;
+        String[] arr1 = new String[map.size()];
+        String[] arr2 = new String[map.size()];
+        int i=0;
+        for(String s : map.keySet()){
+            arr1[i] = s;
+            String [] arr = str.split(",");
+            Arrays.sort(arr);
+            String newS = coop(arr);
+            arr2[i] = newS;
+            i++;
+        }
+        return true;
+    }
 
-        //System.out.println(new Test().unequalTriplets(arr1));
+    public static String coop(String[] arr){
+        String res = "";
+        for(int i =0;i<arr.length;i++){
+            res=res+arr[i]+",";
+        }
+        return res.substring(0,res.length()-1);
     }
 }
--- src/com/test/tt.java
+++ src/com/test/tt.java
@@ -1,180 +1,193 @@
 package com.test;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-/**
- * 长度不超过nn，且包含子序列“us”的、只由小写字母构成的字符串有多少个？ 答案对10^9+710
- * 9
- *  +7取模。
- * 所谓子序列，指一个字符串删除部分字符（也可以不删）得到的字符串。
- * 例如，"unoacscc"包含子序列"us"，但"scscucu"则不包含子序列"us"
- */
 public class tt{
-    //public static void main(String[] args) {
-    //    Scanner sc = new Scanner(System.in);
-    //    int n = sc.nextInt();
-    //    long[][] dp = new long[n + 1][3];
-    //    // dp[i][0] 代表长度为i的字符串没有u
-    //    // dp[i][1] 代表长度为i的字符串有u没有s
-    //    dp[1][0] = 25;
-    //    dp[1][1] = 1;
-    //
-    //    long ans = 0;
-    //    for (int i = 2; i <= n; i++) {
-    //        dp[i][0] = dp[i-1][0] * 25 % 1000000007;
-    //        dp[i][1] = (dp[i-1][0] + dp[i-1][1] * 25 % 1000000007) % 1000000007;
-    //        dp[i][2] = (dp[i-1][1] + dp[i-1][2] * 26 % 1000000007) % 1000000007;
-    //        ans = (ans + dp[i][2]) % 1000000007;
-    //    }
-    //
-    //    System.out.println(ans);
-    //
-
-    /*public static void main(String[] args) {
-        String line = "W\0O\0R\0K\0G\0R\0O\0U\0P\0";
-        char[] charArray = line.toCharArray();
-        for (char c : charArray) {
-            System.out.println(c + ": " + (int) c);
-        }
-        System.out.println(line.length()); // 9
-        line = line.replace(String.valueOf((char) 61557), "");
-        line = line.replace(String.valueOf((char) 61656), "");
-        line = line.replace(String.valueOf((char) 61550), "");
-        line = line.replace(String.valueOf((char) 61599), "");
-        line = line.replace(String.valueOf((char) 61548), "");
-        line = line.replace(String.valueOf((char) 61672), "");
-        line = line.replace(String.valueOf((char) 8226), "");
-        line = line.replace(String.valueOf((char) 8203), "");
-        line = line.replace(String.valueOf((char) 160), "");
-        System.out.println("line: "+line.length()); // 0
-        System.out.println(line);
-    }*/
-    /*public static void main(String[] args) {
-        System.out.println(System.currentTimeMillis());
-        System.out.println(Calendar.YEAR);
-        System.out.println(Calendar.MONTH);
-        System.out.println(Calendar.DAY_OF_WEEK);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String s = sdf.format(new Date());
-        System.out.println(new Date());
-        System.out.println(s);
-        System.out.println("ss-"+(4.5*15.5));
-        String [] str = s.split("-");
-        for(int i=0;i<str.length;i++){
-            System.out.println(str[i]);
-        }
-    }*/
-    /*public static void main(String[] args) {
-        int[] nums = {1,2,3,1};
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(!map.keySet().contains(nums[i])){
-                map.put(nums[i],1);
-            }else{
-                map.put(nums[i],map.get(nums[i])+1);
-            }
-        }
-        int count=0;
-        for(int i :map.keySet()){
-            if(map.get(i)>=1){
-                count++;
-            }
-        }
-        *//*if(count>=1){
-            return true;
-        }else {
-            return false;
-        }*//*
-        System.out.println(map);
-    }*/
-    /*public int distMoney(int money, int children) {
-        if (money < children) {
-            return -1;
-        }
-        money -= children;
-        int cnt = Math.min(money / 7, children);
-        money -= cnt * 7;
-        children -= cnt;
-        if ((children == 0 && money > 0) || (children == 1 && money == 3)) {
-            cnt--;
-        }
-        return cnt;
-    }
-
     public static void main(String[] args) {
-        System.out.println(new tt().distMoney(9,10));
-    }*/
-    public static void main(String[] args) {
-        System.out.println(15.5/12.0*4.5);
-        System.out.println(solution());
+        List list = List.of("aa","bb");
+        Set set = Set.of("aa","bb");
+        Map map = Map.of("aa",1,"bb",2);
+        System.out.println("11");
     }
-    /*public static int solution(){
-        String[] words = {"qwer","asdfgh","k"};
-        Arrays.sort(words,(s1,s2) -> {
-            return s1.length() - s2.length();
-        });
-        Arrays.sort(words, (s1, s2) -> {return s1.length() - s2.length();});
-        Arrays.sort(words, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return 0;
-            }
-        });
-        return 0;
-    }*/
-    public static String solution(){
-        Integer[] arr1 = {4,1,2,9,55,64,25};
-        Integer[] arr2 = {4,1,2,9,55,64,25};
-        Integer[] arr3 = {4,1,2,9,55,64,25};
-        String[] words = {"qwer","asdfgh","k"};
-        Arrays.sort(arr1,new Comparator<Integer>(){
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o1-o2;
-            }
-        });
-        Arrays.sort(arr2, (o1, o2) -> o1-o2);
-        Arrays.sort(arr3, Comparator.comparingInt(o -> o));
-
-        String str1 = "";
-        String str2 = "";
-        String str3 = "";
-        for(int i=0;i<arr1.length-1;i++){
-            str1+=arr1[i]+",";
-        }
-        str1+=arr1[arr1.length-1];
-        for(int i=0;i<arr2.length-1;i++){
-            str2+=arr2[i]+",";
-        }
-        str2+=arr2[arr2.length-1];
-        for(int i=0;i<arr3.length-1;i++){
-            str3+=arr3[i]+",";
-        }
-        str3+=arr3[arr3.length-1];
-        return str1+"\n"+str2+"\n"+str3+"\n";
-    }
-
 }
-/**
- * {
- * 	"name": "Fear and Greed Index",
- * 	"data": [
- * fng_value,fng_classification,date
- * 2023-10-19,52,Neutral
- * 2023-10-18,50,Neutral
- * 2023-10-17,52,Neutral
- * 2023-10-16,47,Neutral
- * 2023-10-15,45,Fear
- * 2023-10-14,47,Neutral
- * 2023-10-13,44,Fear
- * 2023-10-12,45,Fear
- * 2023-10-11,47,Neutral
- * 2023-10-10,50,Neutral
- * 	],
- * 	"metadata": {
- * 		"error": null
- *        }
- * }
- */
-
+//
+//import java.text.SimpleDateFormat;
+//import java.util.*;
+//
+///**
+// * 长度不超过nn，且包含子序列“us”的、只由小写字母构成的字符串有多少个？ 答案对10^9+710
+// * 9
+// *  +7取模。
+// * 所谓子序列，指一个字符串删除部分字符（也可以不删）得到的字符串。
+// * 例如，"unoacscc"包含子序列"us"，但"scscucu"则不包含子序列"us"
+// */
+//public class tt{
+//    //public static void main(String[] args) {
+//    //    Scanner sc = new Scanner(System.in);
+//    //    int n = sc.nextInt();
+//    //    long[][] dp = new long[n + 1][3];
+//    //    // dp[i][0] 代表长度为i的字符串没有u
+//    //    // dp[i][1] 代表长度为i的字符串有u没有s
+//    //    dp[1][0] = 25;
+//    //    dp[1][1] = 1;
+//    //
+//    //    long ans = 0;
+//    //    for (int i = 2; i <= n; i++) {
+//    //        dp[i][0] = dp[i-1][0] * 25 % 1000000007;
+//    //        dp[i][1] = (dp[i-1][0] + dp[i-1][1] * 25 % 1000000007) % 1000000007;
+//    //        dp[i][2] = (dp[i-1][1] + dp[i-1][2] * 26 % 1000000007) % 1000000007;
+//    //        ans = (ans + dp[i][2]) % 1000000007;
+//    //    }
+//    //
+//    //    System.out.println(ans);
+//    //
+//
+//    /*public static void main(String[] args) {
+//        String line = "W\0O\0R\0K\0G\0R\0O\0U\0P\0";
+//        char[] charArray = line.toCharArray();
+//        for (char c : charArray) {
+//            System.out.println(c + ": " + (int) c);
+//        }
+//        System.out.println(line.length()); // 9
+//        line = line.replace(String.valueOf((char) 61557), "");
+//        line = line.replace(String.valueOf((char) 61656), "");
+//        line = line.replace(String.valueOf((char) 61550), "");
+//        line = line.replace(String.valueOf((char) 61599), "");
+//        line = line.replace(String.valueOf((char) 61548), "");
+//        line = line.replace(String.valueOf((char) 61672), "");
+//        line = line.replace(String.valueOf((char) 8226), "");
+//        line = line.replace(String.valueOf((char) 8203), "");
+//        line = line.replace(String.valueOf((char) 160), "");
+//        System.out.println("line: "+line.length()); // 0
+//        System.out.println(line);
+//    }*/
+//    /*public static void main(String[] args) {
+//        System.out.println(System.currentTimeMillis());
+//        System.out.println(Calendar.YEAR);
+//        System.out.println(Calendar.MONTH);
+//        System.out.println(Calendar.DAY_OF_WEEK);
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        String s = sdf.format(new Date());
+//        System.out.println(new Date());
+//        System.out.println(s);
+//        System.out.println("ss-"+(4.5*15.5));
+//        String [] str = s.split("-");
+//        for(int i=0;i<str.length;i++){
+//            System.out.println(str[i]);
+//        }
+//    }*/
+//    /*public static void main(String[] args) {
+//        int[] nums = {1,2,3,1};
+//        Map<Integer,Integer> map = new HashMap<>();
+//        for(int i=0;i<nums.length;i++){
+//            if(!map.keySet().contains(nums[i])){
+//                map.put(nums[i],1);
+//            }else{
+//                map.put(nums[i],map.get(nums[i])+1);
+//            }
+//        }
+//        int count=0;
+//        for(int i :map.keySet()){
+//            if(map.get(i)>=1){
+//                count++;
+//            }
+//        }
+//        *//*if(count>=1){
+//            return true;
+//        }else {
+//            return false;
+//        }*//*
+//        System.out.println(map);
+//    }*/
+//    /*public int distMoney(int money, int children) {
+//        if (money < children) {
+//            return -1;
+//        }
+//        money -= children;
+//        int cnt = Math.min(money / 7, children);
+//        money -= cnt * 7;
+//        children -= cnt;
+//        if ((children == 0 && money > 0) || (children == 1 && money == 3)) {
+//            cnt--;
+//        }
+//        return cnt;
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(new tt().distMoney(9,10));
+//    }*/
+//    public static void main(String[] args) {
+//        System.out.println(15.5/12.0*4.5);
+//        System.out.println(solution());
+//    }
+//    /*public static int solution(){
+//        String[] words = {"qwer","asdfgh","k"};
+//        Arrays.sort(words,(s1,s2) -> {
+//            return s1.length() - s2.length();
+//        });
+//        Arrays.sort(words, (s1, s2) -> {return s1.length() - s2.length();});
+//        Arrays.sort(words, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return 0;
+//            }
+//        });
+//        return 0;
+//    }*/
+//    public static String solution(){
+//        Integer[] arr1 = {4,1,2,9,55,64,25};
+//        Integer[] arr2 = {4,1,2,9,55,64,25};
+//        Integer[] arr3 = {4,1,2,9,55,64,25};
+//        String[] words = {"qwer","asdfgh","k"};
+//        Arrays.sort(arr1,new Comparator<Integer>(){
+//            @Override
+//            public int compare(Integer o1, Integer o2) {
+//                return o1-o2;
+//            }
+//        });
+//        Arrays.sort(arr2, (o1, o2) -> o1-o2);
+//        Arrays.sort(arr3, Comparator.comparingInt(o -> o));
+//
+//        String str1 = "";
+//        String str2 = "";
+//        String str3 = "";
+//        for(int i=0;i<arr1.length-1;i++){
+//            str1+=arr1[i]+",";
+//        }
+//        str1+=arr1[arr1.length-1];
+//        for(int i=0;i<arr2.length-1;i++){
+//            str2+=arr2[i]+",";
+//        }
+//        str2+=arr2[arr2.length-1];
+//        for(int i=0;i<arr3.length-1;i++){
+//            str3+=arr3[i]+",";
+//        }
+//        str3+=arr3[arr3.length-1];
+//        return str1+"\n"+str2+"\n"+str3+"\n";
+//    }
+//
+//}
+///**
+// * {
+// * 	"name": "Fear and Greed Index",
+// * 	"data": [
+// * fng_value,fng_classification,date
+// * 2023-10-19,52,Neutral
+// * 2023-10-18,50,Neutral
+// * 2023-10-17,52,Neutral
+// * 2023-10-16,47,Neutral
+// * 2023-10-15,45,Fear
+// * 2023-10-14,47,Neutral
+// * 2023-10-13,44,Fear
+// * 2023-10-12,45,Fear
+// * 2023-10-11,47,Neutral
+// * 2023-10-10,50,Neutral
+// * 	],
+// * 	"metadata": {
+// * 		"error": null
+// *        }
+// * }
+// */
+//
